1488b6cb29401ea83ef6e4d6367f23e31aef0800
Vladislav Marinin <czargion@gmail.com>
2023-04-06 03:51:25 +0300

    resolved all tasks

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { contacts: [] };
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact(state, { payload }) {
+      state.contacts.push(payload);
+    },
+    deleteContact(state, { payload }) {
+      const index = state.contacts.findIndex(contact => contact.id === payload);
+      state.contacts.splice(index, 1);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export const contactsReducer = contactsSlice.reducer;
